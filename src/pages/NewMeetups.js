21b0711b3228396bf784ccbe2fefdffa73e247c7
Eduardo Quintana <eduardoq@dentalselect.com>
2021-05-07 16:01:55 -0600

    Initial commit

---
+++ src/pages/NewMeetups.js
@@ -0,0 +1,29 @@
+import { useHistory } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+const NewMeetup = () => {
+  const history = useHistory();
+
+  const addMeetupHandler = (meetupData) => {
+    fetch(
+      "https://react-meetup-app-d1912-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  };
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+};
+
+export default NewMeetup;
